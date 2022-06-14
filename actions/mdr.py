9c93a105590c8b2f3bf4b19ea05c71fd7238ad93
JoaoPeriquito <48806417+JoaoPeriquito@users.noreply.github.com>
2022-06-14 09:49:53 +0100

    - Multi Processing was removed (for now) - MDR was successfully tested for single slice ( _mdr function was adapted to multi-slice - MDR was successfully tested for multi-slice Successfully tested means MDR of T1, T2, T2*, DWI, DTI, DCE and MT ran (and results appeared on the GUI) with a precision of 100 and a max iterations of 2 (for speed). Further tests will be carried out in a desktop with precision = 1 and max iterations of 5.

--- actions/mdr.py
+++ actions/mdr.py
@@ -3,11 +3,18 @@ import numpy as np
 
 import weasel
 import mdreg
+import mdreg.models.T2star_simple
+import mdreg.models.T2_simple
+import mdreg.models.T1_simple
+import mdreg.models.DWI_simple
+import mdreg.models.DTI
+import mdreg.models.DCE_2CFM
+
+
 
 # TODO: set fixed colour scales for exported parameter maps
 
-elastix_pars = os.path.join(os.path.dirname(__file__), 'elastix')
-
+elastix_pars = os.path.join(os.path.join(os.path.dirname(__file__)).split("actions")[0], 'elastix')
 
 class MDRegConst(weasel.Action):
     """Perform MDR on all slices using a constant model"""
@@ -43,20 +50,20 @@ class MDRegConst(weasel.Action):
 
 
 class MDRegT2star(weasel.Action):
-
+    
     def run(self, app, series=None):
 
         if series is None:
             series = app.get_selected(3)[0]
 
         array, header = series.array(['SliceLocation', 'EchoTime'], pixels_first=True)
-        slice = 2
+        signal_pars = 0
+        signal_model = mdreg.models.T2star_simple
+        elastix_file = 'BSplines_T2star.txt'
+        number_slices = array.shape[2]
 
-        signal_pars = [hdr.EchoTime for hdr in header[slice,:,0]]
-        signal_model = mdreg.models.T2star
-        elastix_file = 'BSplines_T2.txt'
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='EchoTime')
 
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
 
 
 class MDRegT2(weasel.Action):
@@ -67,13 +74,13 @@ class MDRegT2(weasel.Action):
             series = app.get_selected(3)[0]
 
         array, header = series.array(['SliceLocation', 'AcquisitionTime'], pixels_first=True)
-        slice = 2
 
         signal_pars = [0,30,40,50,60,70,80,90,100,110,120]
-        signal_model = mdreg.models.T2
+        signal_model = mdreg.models.T2_simple
         elastix_file = 'BSplines_T2.txt'
-
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
+        number_slices = array.shape[2]
+        
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='None')
 
 
 class MDRegT1(weasel.Action):
@@ -84,13 +91,13 @@ class MDRegT1(weasel.Action):
             series = app.get_selected(3)[0]
 
         array, header = series.array(['SliceLocation', 'InversionTime'], pixels_first=True)
-        slice = 2
 
-        signal_pars = [hdr.InversionTime for hdr in header[slice,:,0]]
+        signal_pars = 0
         signal_model = mdreg.models.T1_simple
         elastix_file = 'BSplines_T1.txt'
+        number_slices = array.shape[2]
 
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='InversionTime')
 
 
 class MDRegDWI(weasel.Action):
@@ -101,13 +108,13 @@ class MDRegDWI(weasel.Action):
             series = app.get_selected(3)[0]
 
         array, header = series.array(['SliceLocation', 'AcquisitionTime'], pixels_first=True)
-        slice = 15
 
         signal_pars = [0,10.000086, 19.99908294, 30.00085926, 50.00168544, 80.007135, 100.0008375, 199.9998135, 300.0027313, 600.0]
-        signal_model = mdreg.models.DWI
+        signal_model = mdreg.models.DWI_simple
         elastix_file = 'BSplines_IVIM.txt'
 
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
+        number_slices = array.shape[2]
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='None')
 
 class MDRegDTI(weasel.Action):
 
@@ -117,17 +124,13 @@ class MDRegDTI(weasel.Action):
             series = app.get_selected(3)[0]
 
         array, header = series.array(['SliceLocation', 'AcquisitionTime'], pixels_first=True)
-        slice = 15
 
-        b_values = [float(hdr[(0x19, 0x100c)]) for hdr in header[slice,:,0]]
-        b_vectors = [hdr[(0x19, 0x100e)] for hdr in header[slice,:,0]]
-        orientation = [hdr.ImageOrientationPatient for hdr in header[slice,:,0]] 
-
-        signal_pars = [b_values, b_vectors, orientation]
+        signal_pars = 0
         signal_model = mdreg.models.DTI
         elastix_file = 'BSplines_DTI.txt'
+        number_slices = array.shape[2]
 
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='DTI')
 
 
 class MDRegMT(weasel.Action):
@@ -156,13 +159,14 @@ class MDRegMT(weasel.Action):
         array = np.concatenate((array_off, array_on), axis=3)
         header = np.concatenate((header_off, header_on), axis=1)
 
-        slice = 7
+        #slice = 7
 
         signal_pars = []
         signal_model = mdreg.models.constant
         elastix_file = 'BSplines_MT.txt'
 
-        _mdr(app, source, slice, array, header, signal_model, signal_pars, elastix_file)
+        number_slices = array.shape[2]
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='None')
 
 
 class MDRegDCE(weasel.Action):
@@ -173,58 +177,89 @@ class MDRegDCE(weasel.Action):
             series = app.get_selected(3)[0]
         array, header = series.array(['SliceLocation', 'AcquisitionTime'], pixels_first=True)
 
-        slice = 4
-
-        # GET AIF
-        for sery in app.folder.series():
-            if sery.SeriesDescription == 'DCE_ART':
-                mask, _ = sery.array()
-                loc = sery.SliceLocation
-                break
-        aif = []
-        for z in range(array.shape[2]):
-            if header[z,0,0].SliceLocation == loc:
-                for t in range(array.shape[3]): #loop to average ROIs
-                    tmask = np.squeeze(array[:,:,:,t,0]) * np.squeeze(mask)
-                    aif.append = np.median(tmask[tmask!=0])
-        time = [hdr.AcquisitionTime for hdr in header[slice,:,0]]
-        time -= time[0]
-               
-        baseline = 15
-        hematocrit = 0.45
-
-        signal_pars = [aif, time, baseline, hematocrit]
+        signal_pars = 0
         signal_model = mdreg.models.DCE_2CFM
         elastix_file = 'BSplines_DCE.txt'
 
-        _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file)
+        number_slices = array.shape[2]
+        _mdr(app, series, number_slices, array, header, signal_model, elastix_file, signal_pars, sort_by='DCE')
 
 
-def _mdr(app, series, slice, array, header, signal_model, signal_pars, elastix_file):
+def _mdr(app, series, number_slices, array, header, signal_model, elastix_file,signal_pars,sort_by):
 
     # PERFORM MDR
-    mdr = mdreg.MDReg()
-    mdr.set_array(np.squeeze(array[:,:,slice,:,0]))
-    mdr.signal_parameters = signal_pars
-    mdr.pixel_spacing = header[slice,0,0].PixelSpacing
-    mdr.signal_model = signal_model
-    mdr.read_elastix(os.path.join(elastix_pars, elastix_file))
-    mdr.status = app.status
-    mdr.pinned_message = 'MDR for slice ' + str(slice+1)
-    mdr.fit()   
+    parameters = signal_model.pars()
+
+    model_fit = np.empty(array.shape)
+    coreg = np.empty(array.shape)
+    pars = np.empty(array.shape[:3] + (len(parameters),) )
+
+    for slice in range(number_slices):
+
+        mdr = mdreg.MDReg()
+
+        if signal_pars!=0:                                                       #if condition because DCE needs to specify parameters apriori (hematocric, aif, baseline)
+            mdr.signal_parameters = signal_pars
+        else:
+            if sort_by == "DTI":
+                        b_values = [float(hdr[(0x19, 0x100c)]) for hdr in header[slice,:,0]]
+                        b_vectors = [hdr[(0x19, 0x100e)] for hdr in header[slice,:,0]]
+                        orientation = [hdr.ImageOrientationPatient for hdr in header[slice,:,0]] 
+                        mdr.signal_parameters = [b_values, b_vectors, orientation]
+            elif sort_by =="DCE" and signal_pars==0:
+                        # GET AIF
+                        for sery in app.folder.series():
+                            if 'DCE_ART' in sery.SeriesDescription:
+                                mask, _ = sery.array()
+                                loc = sery.SliceLocation
+                                break
+                        aif = []
+                        for z in range(array.shape[2]):
+                            if header[z,0,0].SliceLocation == loc:
+                                for t in range(array.shape[3]): #loop to average ROIs
+                                    tmask = np.squeeze(array[:,:,z,t,0]) * np.squeeze(mask)
+                                    aif.append(np.median(tmask[tmask!=0]))
+
+                        time = [hdr.AcquisitionTime for hdr in header[slice,:,0]]
+                        #time -= time[0]
+                        time = time[1:]
+               
+                        baseline = 15
+                        hematocrit = 0.45
+                        signal_pars = [aif, time, baseline, hematocrit]
+                        mdr.signal_parameters = signal_pars
+
+            else:
+                mdr.signal_parameters = [hdr[sort_by] for hdr in (header[slice,:,0])]
+            
+        mdr.set_array(np.squeeze(array[:,:,slice,:,0]))     
+        mdr.pixel_spacing = header[slice,0,0].PixelSpacing
+        mdr.signal_model = signal_model
+        mdr.read_elastix(os.path.join(elastix_pars, elastix_file))
+        mdr.status = app.status
+        mdr.pinned_message = 'MDR for slice ' + str(slice+1)
+        mdr.fit()
+        
+        model_fit[:,:,slice,:,0] = mdr.model_fit
+        coreg[:,:,slice,:,0] = mdr.coreg
+        pars[:,:,slice,:] = mdr.pars
+
+
+   # array = [x,y,z,TE]
+   # pars = [x,y,z,2]
+   # model_fit = [x,y,z,TE]
+   # coreg = [x,y,z,TE]
     
-    # SAVE RESULTS AS DICOM
-    parameters = signal_model.par()
     for p in range(len(parameters)):
         par = series.SeriesDescription + '_mdr_par_' + parameters[p]
         par = series.new_sibling(SeriesDescription=par)
-        par.set_array(np.squeeze(mdr.par[...,p]), np.squeeze(header[slice,...]), pixels_first=True)
+        par.set_array(np.squeeze(pars[...,p]), np.squeeze(header[:,0]), pixels_first=True)
     fit = series.SeriesDescription + '_mdr_fit'
     fit = series.new_sibling(SeriesDescription=fit)
-    fit.set_array(mdr.model_fit, np.squeeze(header[slice,...]), pixels_first=True)
+    fit.set_array(model_fit, np.squeeze(header[:,:]), pixels_first=True)
     moco = series.SeriesDescription + '_mdr_moco'
     moco = series.new_sibling(SeriesDescription = moco)
-    moco.set_array(mdr.coreg, np.squeeze(header[slice,...]), pixels_first=True)
+    moco.set_array(coreg, np.squeeze(header[:,:]), pixels_first=True)
 
     # DISPLAY RESULTS
     #app.display(moco) 
