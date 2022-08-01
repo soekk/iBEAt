51b7d55aa4bb611857e2d9eac22e651c83b700f1
JoaoPeriquito <joao.s.periquito@gmail.com>
2022-08-01 10:52:16 +0100

    added virtual env (in comments)

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# To develop the application
+# --------------------------
+# py -3 -m venv .venv           # create virtual environment
+# .venv/Scripts/activate        # activate virtual environment
+
 import weasel
 from weasel.apps.dicom import Windows
 
