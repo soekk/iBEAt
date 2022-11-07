9eeda23d8103c5518118d3dc072bcd06feee1e92
Joao Dos Santos Periquito <j.s.periquito@sheffield.ac.uk>
2022-11-07 16:42:03 +0000

    Update iBEAT.sh

--- Scripts/iBEAt_cluster/iBEAT.sh
+++ Scripts/iBEAt_cluster/iBEAT.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Request 8 gigabytes of real memory (mem)
 #####$ -l rmem=2GB
-# Request 8  cores in an OpenMP environment
-#$ -pe openmp 8
+# Request 16  cores in an OpenMP environment
+#$ -pe openmp 16
 # Email notifications to j.s.periquito@sheffield.ac.uk
 #$ -M j.s.periquito@sheffield.ac.uk
 # Email notifications if the job aborts
 #$ -m a
 # Name the job
-#$ -N iBEAT
+#$ -N iBEAt_Leeds_01_to_02
 # Request 95 hours of time
-#$ -l h_rt=95:00:00
+#$ -l h_rt=95:58:00
+#$ -t 1-2
 
 module load apps/python/conda
 
@@ -21,4 +22,4 @@ export OMP_NUM_THREADS=$NSLOTS
 
 # it is assumed that the conda environment 'myscience' has already been created
 conda activate myscience
-python main_cluster.py
+python main_cluster.py --num $SGE_TASK_ID
