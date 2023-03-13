489e350a3639899ccebfc76651fde674ffabd5b6
plaresmedima <steven.sourbron@googlemail.com>
2023-03-13 14:59:08 +0000

    Spring clean of repository

--- hook-sklearn.py
+++ hook-sklearn.py
@@ -1,3 +1,6 @@
+from PyInstaller.utils.hooks import collect_data_files
+datas = collect_data_files('sklearn')
+
 hiddenimports = [
     "sklearn.metrics._pairwise_distances_reduction._middle_term_computer", 
     "sklearn.metrics._pairwise_distances_reduction._datasets_pair"
--- models/iBEAt_Model_Library/README.md
+++
@@ -1,4 +0,0 @@
-# iBEAt_Model_Library
-
-Core image processing functions used in iBEAt pipeline
-Python Library of model functions
